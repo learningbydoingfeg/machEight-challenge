e547eacc79c9de7950ab816fa9f47ba1a07f5efb
Felipe Estrada <felipe.estradagomez@gmail.com>
2022-11-30 15:06:30 -0500

    pushing code from local

---
+++ index.js
@@ -0,0 +1,22 @@
+const app = (list, val) => {
+  
+
+  let mylist = list.sort((a, b) => parseFloat(b) - parseFloat(a));
+  let response = [];
+
+  for (let i = 0; i < mylist.length; i++) {
+
+    if (mylist.includes(val - mylist[i])) {
+      response.push([mylist[i], val - mylist[i]]);
+      mylist.splice(mylist[i], 1);
+      mylist.splice(mylist.indexOf(val - mylist[i]), 1);
+    }
+  }
+
+  console.log(response)
+};
+
+const list = [1, 9, 5, 0, 20, -4, 12, 16, 7];
+const desiredValue = 12;
+
+app(list, desiredValue);
